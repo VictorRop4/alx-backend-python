833fb76434d556c9b1455881f7fd7a427b7f0801
VictorRop4 <victorrop99@gmail.com>
2025-07-27 12:55:43 -0700

    Robust APIs

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import User, Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        participant_ids = request.data.get('participants', [])
+        conversation = Conversation.objects.create()
+        conversation.participants.set(participant_ids)
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        sender_id = request.data.get('sender')
+        conversation_id = request.data.get('conversation')
+        message_body = request.data.get('message_body')
+
+        try:
+            sender = User.objects.get(user_id=sender_id)
+            conversation = Conversation.objects.get(conversation_id=conversation_id)
+        except (User.DoesNotExist, Conversation.DoesNotExist):
+            return Response({'error': 'Invalid sender or conversation ID'}, status=400)
+
+        message = Message.objects.create(sender=sender, conversation=conversation, message_body=message_body)
+        serializer = self.get_serializer(message)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
