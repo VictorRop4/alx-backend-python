77fbf9ae9b2bcd94d39894616fbb978970d99232
VictorRop4 <victorrop99@gmail.com>
2025-10-04 19:13:58 +0300

    User permission

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -4,9 +4,16 @@ class IsParticipantOfConversation(permissions.BasePermission):
     """
     Only participants can access a conversation or its messages.
     """
+    def has_object_permission(self, request, view):
+            return bool(request.user and request.user.Isauthenticated)
+    
     def has_object_permission(self, request, view, obj):
-        if hasattr(obj, "participants"):  # Conversation
-            return request.user in obj.participants.all()
-        if hasattr(obj, "conversation"):  # Message
-            return request.user in obj.conversation.participants.all()
+        # Step 2: Allow access only if the user is a participant
+        if hasattr(obj, "participants"):  # Conversation instance
+            return obj.participants.filter(id=request.user.id).exists()
+
+        if hasattr(obj, "conversation"):  # Message instance
+            return obj.conversation.participants.filter(id=request.user.id).exists()
+
         return False
+
