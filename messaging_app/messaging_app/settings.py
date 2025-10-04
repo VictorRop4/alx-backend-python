97566fabcb3bda23dda637edd2bd11d7d05e97da
VictorRop4 <victorrop99@gmail.com>
2025-10-04 23:13:48 +0300

    Settings.py Debugging

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
 
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
